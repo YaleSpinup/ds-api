e43006121f7d66c667dff9de61a2e04abdccd72a
Tenyo Grozev <tenyo.grozev@yale.edu>
2020-03-03 13:26:54 -0500

    test delete

--- s3datarepository/s3datarepository_test.go
+++ s3datarepository/s3datarepository_test.go
@@ -2,10 +2,13 @@ package s3datarepository
 
 import (
 	"context"
+	"errors"
+	"reflect"
 	"strings"
 	"testing"
 	"time"
 
+	"github.com/YaleSpinup/ds-api/apierror"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/request"
@@ -112,5 +115,67 @@ func TestDeprovision(t *testing.T) {
 }
 
 func TestDelete(t *testing.T) {
-	t.Log("TODO")
+	s := S3Repository{
+		S3: newMockS3Client(t, nil),
+	}
+
+	// test success
+	err := s.Delete(context.TODO(), "68004EEC-6044-45C9-91E5-AF836DCD9234")
+	if err != nil {
+		t.Errorf("expected nil error, got: %s", err)
+	}
+
+	// test empty id
+	err = s.Delete(context.TODO(), "")
+	if aerr, ok := err.(apierror.Error); ok {
+		if aerr.Code != apierror.ErrBadRequest {
+			t.Errorf("expected error code %s, got: %s", apierror.ErrBadRequest, aerr.Code)
+		}
+	} else {
+		t.Errorf("expected apierror.Error, got: %s", reflect.TypeOf(err).String())
+	}
+
+	// test ErrCodeNoSuchBucket
+	s.S3.(*mockS3Client).err = awserr.New(s3.ErrCodeNoSuchBucket, "bucket not found", nil)
+	err = s.Delete(context.TODO(), "68004EEC-6044-45C9-91E5-AF836DCD9234")
+	if aerr, ok := err.(apierror.Error); ok {
+		if aerr.Code != apierror.ErrNotFound {
+			t.Errorf("expected error code %s, got: %s", apierror.ErrNotFound, aerr.Code)
+		}
+	} else {
+		t.Errorf("expected apierror.Error, got: %s", reflect.TypeOf(err).String())
+	}
+
+	// test NotFound
+	s.S3.(*mockS3Client).err = awserr.New("NotFound", "bucket not found", nil)
+	err = s.Delete(context.TODO(), "68004EEC-6044-45C9-91E5-AF836DCD9234")
+	if aerr, ok := err.(apierror.Error); ok {
+		if aerr.Code != apierror.ErrNotFound {
+			t.Errorf("expected error code %s, got: %s", apierror.ErrNotFound, aerr.Code)
+		}
+	} else {
+		t.Errorf("expected apierror.Error, got: %s", reflect.TypeOf(err).String())
+	}
+
+	// test BucketNotEmpty
+	s.S3.(*mockS3Client).err = awserr.New("BucketNotEmpty", "bucket not empty", nil)
+	err = s.Delete(context.TODO(), "68004EEC-6044-45C9-91E5-AF836DCD9234")
+	if aerr, ok := err.(apierror.Error); ok {
+		if aerr.Code != apierror.ErrConflict {
+			t.Errorf("expected error code %s, got: %s", apierror.ErrConflict, aerr.Code)
+		}
+	} else {
+		t.Errorf("expected apierror.Error, got: %s", reflect.TypeOf(err).String())
+	}
+
+	// test non-aws error
+	s.S3.(*mockS3Client).err = errors.New("things blowing up")
+	err = s.Delete(context.TODO(), "68004EEC-6044-45C9-91E5-AF836DCD9234")
+	if aerr, ok := err.(apierror.Error); ok {
+		if aerr.Code != apierror.ErrInternalError {
+			t.Errorf("expected error code %s, got: %s", apierror.ErrInternalError, aerr.Code)
+		}
+	} else {
+		t.Errorf("expected apierror.Error, got: %s", reflect.TypeOf(err).String())
+	}
 }
